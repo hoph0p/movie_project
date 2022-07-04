16bb9a673823489206cde1233e0fc181cbb5bea4
Hop_Hop <novikov.hop.work@yandex.ru>
2022-07-04 17:53:43 +0300

    end of session commit

--- movie_app/views.py
+++ movie_app/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Movie
+
 
 # Create your views here.
+def show_all_movie(request):
+    movies = Movie.objects.all()
+    data = {
+        'movies': movies,
+    }
+    return render(request, 'movie_app/all_movies.html', context=data)
+
+
+def show_one_movie(request, id_movie: int):
+    movie = get_object_or_404(Movie, id=id_movie)
+    data = {
+        'movie': movie,
+    }
+    return render(request, 'movie_app/one_movie.html', context=data)
