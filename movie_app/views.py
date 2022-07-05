2dc307b34969615a0dfe81870229ef0ea7fc91fa
Hop_Hop <novikov.hop.work@yandex.ru>
2022-07-05 13:28:16 +0300

    end of work

--- movie_app/views.py
+++ movie_app/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Movie
+from django.db.models import F, Sum, Max, Min, Count, Avg
 
 
 # Create your views here.
 def show_all_movie(request):
-    movies = Movie.objects.all()
+    movies = Movie.objects.order_by(F('year').asc(nulls_last=True), 'rating')
+    agg = movies.aggregate(Avg('budget'), Max('rating'), Min('rating'))
     data = {
         'movies': movies,
+        'agg': agg,
+        'total': movies.count(),
     }
     return render(request, 'movie_app/all_movies.html', context=data)
 
